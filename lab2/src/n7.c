4937e828d36deb7c747cb75600c83e351393c00a
Сергей <serge@LAPTOP-9F5GDU8Q.localdomain>
2022-11-20 00:35:16 +0300

    lab2-7 complete

---
+++ lab2/src/n7.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("Calling fork...\n\n");
+
+    int fork_result = fork();
+    if (fork_result == -1)
+    {
+        perror("fork error");
+    }
+
+    if (fork_result == 0)
+    {
+        printf("--- SON ---\n");
+
+        printf("Fork result: %d\n", fork_result);
+        printf("PID: %d\nPPID: %d\nPGRP: %d\n\n",
+               getpid(), getppid(), getpgrp());
+
+        setpgrp();
+        printf("PGRP new: %d", getpgrp());
+
+        pause();
+    }
+    else
+    {
+        int stat_loc;
+
+        int pid = wait(&stat_loc);
+
+        printf("--- FATHER ---\n");
+        printf("Proc %d complete!\nstat_loc: %d\n",
+               pid, stat_loc);
+
+        printf("Fork result: %d\n", fork_result);
+        printf("PID: %d\nPPID: %d\nPGRP: %d\n\n",
+               getpid(), getppid(), getpgrp());
+
+        pause();
+    }
+}
