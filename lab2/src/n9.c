f40994b807b9d1d9993d3e256aa1493b48ff1975
Сергей <serge@LAPTOP-9F5GDU8Q.localdomain>
2022-11-20 22:11:39 +0300

    lab2-9 complete

---
+++ lab2/src/n9.c
@@ -0,0 +1,62 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define BUFSIZ 84
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Invalid args!\nEnter %s <filename>\n", argv[0]);
+        return -1;
+    }
+
+    int fork_result = fork();
+
+    if (fork_result == 0)
+    {
+        int source_fid = open(argv[1], O_RDONLY);
+        if (source_fid == -1)
+        {
+            perror("open perror");
+            return -1;
+        }
+
+        int destination_fid = creat("n9_son_asset", 0644);
+
+        char str_buf[BUFSIZ];
+        read(source_fid, str_buf, BUFSIZ);
+
+        write(destination_fid, str_buf, BUFSIZ);
+        lseek(destination_fid, 0, SEEK_SET);
+        read(destination_fid, str_buf, BUFSIZ);
+
+        write(1, str_buf, BUFSIZ);
+    }
+    else
+    {
+        int source_fid = open(argv[1], O_RDONLY);
+        if (source_fid == -1)
+        {
+            perror("open perror");
+            return -1;
+        }
+
+        int destination_fid = creat("n9_father_asset", 0644);
+
+        char str_buf[BUFSIZ];
+        read(source_fid, str_buf, BUFSIZ);
+
+        write(destination_fid, str_buf, BUFSIZ);
+        lseek(destination_fid, 0, SEEK_SET);
+        read(destination_fid, str_buf, BUFSIZ);
+
+        wait(NULL);
+
+        write(1, str_buf, BUFSIZ);
+    }
+
+    return 0;
+}
