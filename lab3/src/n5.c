b74a982f5cf9d97a20d2c7badfd2d0b32d670659
Сергей <serge@LAPTOP-9F5GDU8Q.localdomain>
2022-12-03 15:04:22 +0300

    lab3-6 complete

--- lab3/src/n5.c
+++ lab3/src/n5.c
@@ -1,10 +1,21 @@
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-int main()
+/* С целью совместить пункты 5 и 6 лабораторной работы,
+я решил получать значение сигнала как аргумент вызова программы  */
+int main(int argc, char *argv[])
 {
+    if (argc > 2)
+    {
+        printf("Invalid args!\n"
+               "Enter %s <sig> or without args\n",
+               argv[0]);
+        return 1;
+    }
+
     int pid = fork();
     if (pid == -1)
     {
@@ -36,13 +47,28 @@ int main()
     }
     else
     {
-        printf("i'm parent/ i want to kill my child...\n");
-        kill(pid, SIGUSR1);
+        printf("i'm parent/"
+               "i want to kill my child...\n");
+
+        int sig = 0;
+        if (argc == 2)
+        {
+            sig = atoi(argv[1]);
+        }
+
+        if (sig == 0)
+        {
+            sig = SIGUSR1;
+        }
+
+        kill(pid, sig);
 
         int stat_loc;
         wait(&stat_loc);
 
-        printf("i'm parent/ my child was completed with status %d\n", stat_loc);
+        printf("i'm parent/"
+               "my child was completed with status %d\n",
+               stat_loc);
     }
 
     return 0;
