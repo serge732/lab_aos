cf1f5ba1fbac1baef2ff672a402b3124cb93bc24
Сергей <serge@LAPTOP-9F5GDU8Q.localdomain>
2022-12-03 14:38:41 +0300

    lab3-4 complete

---
+++ lab3/src/n4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main()
+{
+    int pid = fork();
+    if (pid == -1)
+    {
+        perror("fork error");
+    }
+
+    if (pid == 0)
+    {
+        printf("i'm child\n");
+
+        int i = 0;
+        while (i < 100)
+        {
+            printf("top level loop current value = %d\n", i);
+            i++;
+
+            int j = 0;
+            while (j < 10000)
+            {
+                j++;
+
+                int k = 0;
+                while (k < 10000)
+                {
+                    k++;
+                }
+            }
+        }
+    }
+    else
+    {
+        printf("i'm parent/ i'm waiting for my child...\n");
+
+        int stat_loc;
+        wait(&stat_loc);
+
+        printf("i'm parent/ my child was completed with status %d\n", stat_loc);
+    }
+
+    return 0;
+}
