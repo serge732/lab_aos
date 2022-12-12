92f55ba6713e8903b6a86fd8d067dfdf28f0c6e3
Сергей <serge@MacBook-Air-Sergej.local>
2022-12-12 22:33:54 +0300

    lab4-8 complete

---
+++ lab4/src/n8.c
@@ -0,0 +1,55 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int pid, rdfd, wrfd, rdbytes = 0, wrbytes = 0;
+    char rdbuf[3];
+    struct flock rdlock;
+    rdlock.l_type = F_RDLCK;
+    rdlock.l_whence = SEEK_SET;
+    rdlock.l_start = 0;
+    rdlock.l_len = 0;
+    const char wrmessage[] = "00\n";
+    int writerator = 0;
+
+    pid = fork();
+    switch (pid)
+    {
+    case -1:
+        perror("fork error");
+        return 1;
+
+    case 0:
+        close(rdfd);
+        wrfd = open(argv[1], O_WRONLY);
+        while ((writerator++ < 89))
+        {
+            wrbytes = write(wrfd, wrmessage, sizeof(wrmessage));
+            if (wrbytes == -1)
+            {
+                perror("write error");
+                return 3;
+            }
+        }
+        close(wrfd);
+        break;
+
+    default:
+        rdfd = open(argv[1], O_RDONLY);
+        fcntl(rdfd, F_SETLKW, &rdlock);
+        while ((rdbytes = read(rdfd, rdbuf, sizeof(rdbuf))) > 0)
+        {
+            if (rdbytes == -1)
+            {
+                perror("read error");
+                return 2;
+            }
+            printf("read from file: %s", rdbuf);
+        }
+        break;
+    }
+
+    return 0;
+}
