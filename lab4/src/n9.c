f822ea0407005cf6f6f47861b3a55d7f3df195d2
Сергей <serge@MacBook-Air-Sergej.local>
2022-12-18 21:29:20 +0300

    lab4-9 updated

--- lab4/src/n9.c
+++ lab4/src/n9.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     int pid, rdfd, wrfd, rdbytes = 0, wrbytes = 0;
     char rdbuf[3];
     struct flock rdlock, wrlock;
-    const char wrmessage[] = "00\n";
+    const char wrmessage[] = "00";
     int writerator = 0;
 
     pid = fork();
@@ -23,6 +23,11 @@ int main(int argc, char *argv[])
         wrlock.l_start = 0;
         wrlock.l_len = 0;
         wrfd = open(argv[1], O_WRONLY);
+        if (fcntl(wrfd, F_SETLK, &wrlock) < 0)
+        {
+            perror("wrlock error");
+            return 3;
+        }
         while ((writerator++ < 89))
         {
             wrbytes = write(wrfd, wrmessage, sizeof(wrmessage));
@@ -36,13 +41,16 @@ int main(int argc, char *argv[])
         break;
 
     default:
-        sleep(1);
         rdlock.l_type = F_RDLCK;
         rdlock.l_whence = SEEK_SET;
         rdlock.l_start = 0;
         rdlock.l_len = 0;
         rdfd = open(argv[1], O_RDONLY);
-        fcntl(rdfd, F_SETLKW, &rdlock);
+        if (fcntl(rdfd, F_SETLK, &rdlock) < 0)
+        {
+            perror("rdlock error");
+            return 3;
+        }
         while ((rdbytes = read(rdfd, rdbuf, sizeof(rdbuf))) > 0)
         {
             if (rdbytes == -1)
