570f3af3be594de76e5bfc1d54bc39e12a704b45
Сергей <serge@MacBook-Air-Sergej.local>
2022-12-18 21:54:05 +0300

    lab4-11 updated

--- lab4/src/n11.c
+++ lab4/src/n11.c
@@ -30,16 +30,27 @@ int main()
             cld_wrlock.l_start = 0;
             cld_wrlock.l_len = 0;
 
-            fcntl(0, F_SETLKW, cld_rdlock);
-            rdbytes = read(0, buf, sizeof(buf));
-            cld_rdlock.l_type = F_UNLCK;
-            fcntl(0, F_SETLK, cld_rdlock);
+            while (fcntl(0, F_SETLKW, &cld_rdlock) == -1)
+            {
+            }
 
-            fcntl(0, F_SETLKW, cld_wrlock);
+            while (fcntl(1, F_SETLKW, &cld_wrlock) == -1)
+            {
+            }
+
+            rdbytes = read(0, buf, sizeof(buf));
             write(1, cld_message, sizeof(cld_message));
             write(1, buf, rdbytes);
+
+            cld_rdlock.l_type = F_UNLCK;
             cld_wrlock.l_type = F_UNLCK;
-            fcntl(0, F_SETLKW, cld_wrlock);
+
+            if (fcntl(0, F_SETLK, &cld_rdlock) == -1)
+            {
+            }
+            if (fcntl(1, F_SETLKW, &cld_wrlock) == -1)
+            {
+            }
         }
         break;
 
@@ -56,16 +67,28 @@ int main()
             prnt_wrlock.l_start = 0;
             prnt_wrlock.l_len = 0;
 
-            fcntl(0, F_SETLKW, prnt_rdlock);
-            rdbytes = read(0, buf, sizeof(buf));
-            prnt_rdlock.l_type = F_UNLCK;
-            fcntl(0, F_SETLK, prnt_rdlock);
+            while (fcntl(0, F_SETLK, &prnt_rdlock) == -1)
+            {
+            }
 
-            fcntl(0, F_SETLKW, prnt_wrlock);
+            while (fcntl(1, F_SETLK, &prnt_wrlock) == -1)
+            {
+            }
+
+            rdbytes = read(0, buf, sizeof(buf));
             write(1, prnt_message, sizeof(prnt_message));
             write(1, buf, rdbytes);
+
+            prnt_rdlock.l_type = F_UNLCK;
             prnt_wrlock.l_type = F_UNLCK;
-            fcntl(0, F_SETLKW, prnt_wrlock);
+
+            if (fcntl(0, F_SETLK, &prnt_rdlock) == -1)
+            {
+            }
+
+            if (fcntl(1, F_SETLKW, &prnt_wrlock) == -1)
+            {
+            }
         }
         break;
     }
